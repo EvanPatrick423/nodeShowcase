3d31105102823cf01fad2a6aa9fea606738094f4
Doop <PatrickE381@gmail.com>
2022-10-06 15:32:42 -0400

    updating the resource imadges and css of the landing page

--- public/landPage/landPage.js
+++ public/landPage/landPage.js
@@ -7,11 +7,11 @@ resumePageTurn.addEventListener('click', () => changePage());
 
 function changePage () {
   if (turned === false) {
-    resumeImg.style.backgroundImage = "url(landPage/resources/0002.jpg)";
+    resumeImg.style.backgroundImage = "url(landPage/resources/Dev1024_2.jpg)";
     resumePageTurn.textContent = '<';
     turned = true;
   } else {
-    resumeImg.style.backgroundImage = "url(landPage/resources/0001.jpg)";
+    resumeImg.style.backgroundImage = "url(landPage/resources/Dev1024_1.jpg)";
     resumePageTurn.textContent = '>';
     turned = false;
   }
