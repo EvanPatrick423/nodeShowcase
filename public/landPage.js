76a9b8906e0c71e607c5228c04d0864f7354d51b
Doop <PatrickE381@gmail.com>
2022-10-08 17:18:01 -0400

    Getting pixelPad working on the new front page

--- public/landPage.js
+++ public/landPage.js
@@ -1,4 +1,6 @@
 import {manipulateDom} from './theOdinProject/DOM/DOM.js';
+import {listen, createGrid, setInk, toggleButton, eraseAllDivs, rangeSlider} from './theOdinProject/etchasketch/etchasketch.js';
+import {} from './theOdinProject/etchasketch/etchasketch.js';
 
 let show = document.getElementById('show');
 
@@ -6,6 +8,7 @@ function clearShow() {
   while (show.lastChild) {
     show.removeChild(show.firstChild);
   }
+  show.classList.remove('pixelPadBack');
 }
 
 function popNavBar () {
@@ -46,6 +49,10 @@ function popNavBar () {
 
 function showPixelPad () {
   clearShow();
+
+  let show = document.getElementById('show');
+  show.classList.add('pixelPadBack');
+
   let contentHolder = document.createElement('div');
   contentHolder.setAttribute('id','content-holder');
 
@@ -53,10 +60,16 @@ function showPixelPad () {
   buttonHolder.setAttribute('id','button-holder');
 
   let eraser = document.createElement('button');
+  eraser.classList.add('pixelPadButton');
   eraser.setAttribute('id', 'eraser-btn');
+  eraser.addEventListener('click', () => toggleButton());
+  eraser.textContent = 'Toggle Eraser';
 
   let eraseAll = document.createElement('button');
   eraseAll.setAttribute('id','erase-all');
+  eraseAll.classList.add('pixelPadButton');
+  eraseAll.addEventListener('click', () => eraseAllDivs());
+  eraseAll.textContent = 'Erase All';
 
   let colorCard = document.createElement('div');
   colorCard.setAttribute('id','color-card');
@@ -67,6 +80,7 @@ function showPixelPad () {
   let colorSelect = document.createElement('input');
   colorSelect.setAttribute('type','color');
   colorSelect.setAttribute('id','color-select');
+  colorSelect.addEventListener('input', x => setInk(x));
 
   let colorText = document.createElement('span');
   colorText.textContent = 'Pick your color';
@@ -74,20 +88,52 @@ function showPixelPad () {
   let sliderBox = document.createElement('div');
   sliderBox.classList.add('slider-box');
 
-  let rangeSlider = document.createElement('input');
-  rangeSlider.setAttribute('id','range-slider');
-  rangeSlider.setAttribute('type','range');
-  rangeSlider.setAttribute('value','48');
-  rangeSlider.setAttribute('min','1');
-  rangeSlider.setAttribute('max','60');
-  rangeSlider.addEventListener('onChange', () => rangeSlider(this.value));
+  let rangeSliderDiv = document.createElement('input');
+  rangeSliderDiv.setAttribute('id','range-slider');
+  rangeSliderDiv.setAttribute('type','range');
+  rangeSliderDiv.setAttribute('value','48');
+  rangeSliderDiv.setAttribute('min','1');
+  rangeSliderDiv.setAttribute('max','60');
+  rangeSliderDiv.addEventListener('change', () => rangeSlider(rangeSliderDiv.value));
 
   let progressBar = document.createElement('div');
   progressBar.setAttribute('id','progress-bar');
 
+  let rangeText = document.createElement('p');
+  rangeText.setAttribute('id','range-text');
+  rangeText.textContent = 'Grid size: 48 x 48';
 
+  let boardHolder = document.createElement('div');
+  boardHolder.setAttribute('id','board-holder');
 
+  let board = document.createElement('div');
+  board.setAttribute('id','board');
+  board.setAttribute('draggable','false');
 
+  sliderBox.appendChild(rangeSliderDiv);
+  sliderBox.appendChild(progressBar);
+  sliderBox.appendChild(rangeText);
+
+  colorBox.appendChild(colorSelect);
+  colorCard.appendChild(colorBox);
+  colorCard.appendChild(colorText);
+
+  buttonHolder.appendChild(eraser);
+  buttonHolder.appendChild(eraseAll);
+  buttonHolder.appendChild(colorCard);
+  buttonHolder.appendChild(sliderBox);
+
+  boardHolder.appendChild(board);
+
+  contentHolder.appendChild(buttonHolder);
+  contentHolder.appendChild(boardHolder);
+
+  show.appendChild(contentHolder);
+
+  //inital grid creation
+  createGrid(48);
+  //initiate listen function
+  listen();
 }
 
 function showDom () {
--- public/theOdinProject/etchasketch/etchasketch.js
+++ public/theOdinProject/etchasketch/etchasketch.js
@@ -1,76 +1,102 @@
 
-  //create Grid
-  function createGrid(z) {
-    for (let i = 0; i < z; i ++){
-      const row = document.createElement('div');
-      row.style.cssText = 'flex:1 1 0 ; display:flex ;';
-      board.appendChild(row)
+let eraser = false;
 
-      for (let x = 0; x < z; x ++){
-        const gridItem = document.createElement('div');
-        gridItem.classList.add("grid-item");
-        row.appendChild(gridItem);
+//create Grid
+export function createGrid(z) {
+  for (let i = 0; i < z; i ++){
+    const row = document.createElement('div');
+    row.style.cssText = 'flex:1 1 0 ; display:flex ;';
+    board.appendChild(row);
+
+    for (let x = 0; x < z; x ++){
+      const gridItem = document.createElement('div');
+      gridItem.classList.add("grid-item");
+      row.appendChild(gridItem);
+    }
+  }
+}
+//----------------------------------------------
+
+//Grid Slider function
+export function rangeSlider(value) {
+    let gridLabels = document.querySelectorAll('#range-value');
+    for (let i = 0; i < gridLabels.length; i++) {
+        gridLabels[i].textContent = value;
+    }
+    let rangeText = document.getElementById('range-text');
+    rangeText.textContent = 'Grid size: ' + value + ' x ' + value;
+    let z = value;
+    deleteGrid();
+    createGrid(z);
+    listen();
+}
+
+function rangeSliderValue(value) {
+    let gridLabels = document.querySelectorAll('#range-value');
+    for (let i = 0; i < gridLabels.length; i++) {
+        gridLabels[i].textContent = value;
+    }
+}
+
+
+//----------------------------------------------
+//Delete Grid function
+function deleteGrid() {
+  const container = document.querySelector('#board');
+  while (container.firstChild) {
+      container.removeChild(board.lastChild);
+  }
+}
+
+//--------------------------------------------
+//eraser buttons
+export function toggleButton() {
+  let eraserButton = document.getElementById('eraser-btn');
+  if (eraser) {
+    eraser = false;
+    eraserButton.classList.remove('btn-on');
+  } else {
+    eraser = true;
+    eraserButton.classList.add('btn-on');
+  }
+}
+//----------------------------------------------
+//Clear all function
+export function eraseAllDivs() {
+  let items = document.querySelectorAll(".grid-item");
+  for (let i = 0; i < items.length; i++) {
+    items[i].style.backgroundColor = '#ffffff';
+  }
+}
+//----------------------------------------------
+
+//-----------------------------------------------
+//For different colors, set variable to the ink color
+let ink = '#000000'; //default black
+
+export function setInk (x) {
+  ink = x.target.value;
+}
+//--------------------------------------------------
+
+//listening for click or hover on board elements
+export function listen() {
+  let items = document.querySelectorAll(".grid-item");
+  for (let i = 0; i < items.length; i++) {
+    //items[i].classList.add("found"); //check to see if the querySelectorAll is finding my grid items
+    items[i].addEventListener('mousedown', drawOnClick);
+    items[i].addEventListener('mouseenter', drawWhileClicked);
+  }
+  //draws while clicked function
+  function drawWhileClicked(x) {
+    if (mouse > 0) {
+      if (eraser) {
+        x.target.style.backgroundColor = '#ffffff';
+      } else {
+        x.target.style.backgroundColor = ink;
       }
     }
   }
-  //inital grid creation
-  createGrid(48);
-  //----------------------------------------------
-
-  //Grid Slider function
-  function rangeSlider(value) {
-      let gridLabels = document.querySelectorAll('#range-value');
-      for (let i = 0; i < gridLabels.length; i++) {
-          gridLabels[i].textContent = value;
-      }
-      z = value;
-      deleteGrid();
-      createGrid(z);
-      listen();
-  }
-
-  function rangeSliderValue(value) {
-      let gridLabels = document.querySelectorAll('#range-value');
-      for (let i = 0; i < gridLabels.length; i++) {
-          gridLabels[i].textContent = value;
-      }
-  }
-
-
-  //----------------------------------------------
-  //Delete Grid function
-  function deleteGrid() {
-    const container = document.querySelector('#board');
-    while (container.firstChild) {
-        container.removeEventListener('mousedown', drawOnClick);
-        container.removeEventListener('mouseenter', drawWhileClicked);
-        container.removeChild(board.lastChild);
-    }
-  }
-
-  //--------------------------------------------
-  //eraser buttons
-  let eraser = false;
-  const eraserButton = document.querySelector('#eraser-btn');
-  eraserButton.addEventListener('click', () => {
-    if (eraser) {
-      eraser = false;
-      eraserButton.classList.remove('btn-on');
-    } else {
-      eraser = true;
-      eraserButton.classList.add('btn-on');
-    }
-  })
-  //----------------------------------------------
-  //Clear all button
-  const eraseAllButton = document.querySelector('#erase-all');
-  eraseAllButton.addEventListener('click', () => {
-    items = document.querySelectorAll(".grid-item");
-    for (i = 0; i < items.length; i++) {
-      items[i].style.backgroundColor = '#ffffff';
-    }
-  })
-  //----------------------------------------------
   //changes color of grid items
   function drawOnClick(x) {
     if (eraser) {
@@ -89,36 +115,6 @@
   document.body.onmouseup = function() {
     mouse--;
   }
-  //-----------------------------------------------
-  //draws while clicked function
-  function drawWhileClicked(x) {
-    if (mouse > 0) {
-      if (eraser) {
-        x.target.style.backgroundColor = '#ffffff';
-      } else {
-        x.target.style.backgroundColor = ink;
-      }
-    }
-  }
-  //--------------------------------------------------
-  //For different colors, set variable to the ink color
-  let ink = '#000000'; //default black
+}
 
-  const colorPicker = document.querySelector('#color-select');
-  colorPicker.addEventListener("input", x => {
-    ink = x.target.value;
-  })
-
-
-  //listening for click or hover on board elements
-  function listen() {
-    items = document.querySelectorAll(".grid-item");
-    for (i = 0; i < items.length; i++) {
-      //items[i].classList.add("found"); //check to see if the querySelectorAll is finding my grid items
-      items[i].addEventListener('mousedown', drawOnClick);
-      items[i].addEventListener('mouseenter', drawWhileClicked);
-    }
-  }
-  //initiate listen function
-  listen()
-  //---------------------------------------------------
+//---------------------------------------------------
